ac9855c9603cd304495de410b67c5ea5a9e556ab
Tigran S <tigran.simonyan.888@gmail.com>
2017-02-09 16:11:56 +0400

    Housekeeping
    Test 1
    
    Test 2

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -17,7 +17,28 @@ class Auth extends CI_Controller {
 		//This method will have the credentials validation
 		$this->load->library('form_validation');
 
-// test changes // 
+// test changes //
+
+        //TEST TEST TEST TEST TEST 1
+
+		$this->form_validation->set_rules('u-name', 'Username', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('u-password', 'Password', 'trim|required|xss_clean|callback_check_database');
+
+//		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
+//		$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean|callback_check_database');
+
+        if(true){
+            echo 'rebase 1 , commit 1';
+        }
+
+        if(true){
+            echo 'rebase 2 , commit 2';
+        }
+
+        if(true){
+            echo 'rebase 3 , commit 3';
+        }
+// test changes //
 
 		$this->form_validation->set_rules('u-name', 'Username', 'trim|required|xss_clean');
 		$this->form_validation->set_rules('u-password', 'Password', 'trim|required|xss_clean|callback_check_database');
@@ -31,6 +52,8 @@ class Auth extends CI_Controller {
 		else { 	//Go to private area
 			redirect('comments/1', 'refresh');
 		}
+
+        //TEST TEST TEST TEST TEST 2
 
 	}
 
